8aa890ee9d79a3a7b8bf7884425ac5690aeaec4d
molsen89 <166087019+molsen89@users.noreply.github.com>
2025-09-28 13:35:15 -0500

    Constants

---
+++ constants.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+const (
+	PI       = 3.14159
+	LANGUAGE = "Go"
+)
+
+func main() {
+	fmt.Println("Value of PI:", PI)
+	fmt.Println("Programming Language:", LANGUAGE)
+}
